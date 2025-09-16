f198e4e001a85a9cd0e993121841efeef05e8593
luderson2 <ludersoncr7@gmail.com>
2025-09-16 11:40:12 -0400

    commencement du HomeScreen

--- MonProjet/src/screens/HomeScreen.js
+++ MonProjet/src/screens/HomeScreen.js
@@ -4,17 +4,22 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Accueil</Text>
+      <View>
+        <Text>Lists</Text>
+      </View>
+      <Text style={styles.title}>All Tasks</Text>
       <Button title="Aller aux Détails" onPress={() => navigation.navigate('Details')} />
       <Button title="Changez la couleur du BackGround" onPress={() => navigation.navigate('Color')} />
 
       <View style={{ height: 12 }} />
       <Button title="Voir le Compteur" onPress={() => navigation.navigate('Counter')} />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-  title: { fontSize: 22, fontWeight: '600' },
+  container: { flexDirection: 'column' , alignItems: 'center', justifyContent: 'center' },
+  title: { fontSize: 22, fontWeight: '600',  },
+  
 });
