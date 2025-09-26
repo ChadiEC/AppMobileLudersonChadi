dd09ee5d1fa95e2d3cd3aae55c67721a67dc90d5
ChadiEC <1985081@collegemv.qc.ca>
2025-09-25 21:05:53 -0400

    Implementation de DateTimePickerModal pour améliorer la façon dont ont choisis la date d'échéance de la tache et mise en place du code couleur de l'urgence

--- MonProjet/src/screens/HomeScreen.js
+++ MonProjet/src/screens/HomeScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
@@ -10,11 +11,191 @@ export default function HomeScreen({ navigation }) {
 
       <View style={{ height: 12 }} />
       <Button title="Voir le Compteur" onPress={() => navigation.navigate('Counter')} />
+=======
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Modal, ScrollView } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { TaskContext } from '../context/TaskContext';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
+export default function HomeScreen({ navigation }) {
+  const { tasks, addTask, getTodayTasks, getTomorrowTasks, getWeekTasks } = useContext(TaskContext);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [type, setType] = useState('Normal');
+  const [urgency, setUrgency] = useState('Basse');
+
+  const handleConfirmDate = (selectedDate) => {
+    setDate(selectedDate);
+    setDatePickerVisibility(false);
+  };
+
+  const getUrgencyColor = (level) => {
+  if (level === "Haute") return "#E24A4A";   
+  if (level === "Moyenne") return "#4A90E2"; 
+  else return "#4CAF50";                          
+};
+
+  const handleAddTask = () => {
+    if (title.trim() === "") return;
+    addTask({
+      title,
+      description,
+      date: date.toISOString().split("T")[0], // YYYY-MM-DD
+      type,
+      urgency
+    });
+    setTitle("");
+    setDescription("");
+    setDate(new Date());
+    setType("Normal");
+    setUrgency("Basse");
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Lists</Text>
+      </View>
+
+      {/* NAVIGATION BOXES */}
+      <ScrollView>
+        <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('AllTask')}>
+          <Ionicons name="checkmark-circle" size={20} color="#fff" />
+          <Text style={styles.boxText}> All task ({tasks.length}) </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('Today')}>
+          <Ionicons name="today" size={20} color="#fff" />
+          <Text style={styles.boxText}>Today ({getTodayTasks().length}) </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('Tomorrow')}>
+          <Ionicons name="calendar" size={20} color="#fff" />
+          <Text style={styles.boxText}>Tomorrow ({getTomorrowTasks().length})</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('Week')}>
+          <Ionicons name="briefcase" size={20} color="#fff" />
+          <Text style={styles.boxText}>Week ({getWeekTasks().length})</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('Completed')}>
+          <Ionicons name="checkmark-done" size={20} color="#fff" />
+          <Text style={styles.boxText}>Completed</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.box} onPress={() => navigation.navigate('Work')}>
+          <Ionicons name="person" size={20} color="#fff" />
+          <Text style={styles.boxText}>Work</Text>
+        </TouchableOpacity>
+      </ScrollView>
+
+      {/* ADD BUTTON */}
+      <View style={styles.addButtonContainer}>
+        <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
+          <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* MODAL */}
+      <Modal visible={modalVisible} animationType="slide" transparent={true}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={{ fontWeight: 'bold', fontSize: 18, marginBottom: 10 }}>Nouvelle tâche</Text>
+
+            <TextInput
+              placeholder="Titre"
+              value={title}
+              onChangeText={setTitle}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Description"
+              value={description}
+              onChangeText={setDescription}
+              style={styles.input}
+            />
+
+            {/* DATE PICKER */}
+            <Text style={{ marginBottom: 5 }}>Échéance :</Text>
+            <TouchableOpacity
+              style={styles.selector}
+              onPress={() => setDatePickerVisibility(true)}
+            >
+              <Text style={{ fontWeight: "bold" }}>
+                {date ? date.toLocaleDateString() : "Choisir une date"}
+              </Text>
+            </TouchableOpacity>
+
+            <DateTimePickerModal
+              isVisible={isDatePickerVisible}
+              mode="date"
+              onConfirm={handleConfirmDate}
+              onCancel={() => setDatePickerVisibility(false)}
+              themeVariant="light"
+
+            />
+
+            {/* URGENCY */}
+            <Text style={{ marginBottom: 5, marginTop: 10 }}>Urgence :</Text>
+            {["Basse", "Moyenne", "Haute"].map((level) => (
+              <TouchableOpacity
+                key={level}
+                style={[
+                  styles.selector,
+                  urgency === level && { backgroundColor: getUrgencyColor(level) },
+                ]}
+                onPress={() => setUrgency(level)}
+              >
+                <Text style={{
+                  color: urgency === level ? "white" : "black",
+                  fontWeight: "bold",
+                }}>
+                  {level}
+                </Text>
+              </TouchableOpacity>
+            ))}
+
+            {/* BUTTONS */}
+            <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 15 }}>
+              <TouchableOpacity onPress={handleAddTask} style={styles.modalButton}>
+                <Text style={{ color: "white" }}>Ajouter</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => setModalVisible(false)}
+                style={[styles.modalButton, { backgroundColor: "gray" }]}
+              >
+                <Text style={{ color: "white" }}>Annuler</Text>
+              </TouchableOpacity>
+            </View>
+
+          </View>
+        </View>
+      </Modal>
+>>>>>>> Stashed changes
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+<<<<<<< Updated upstream
   container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
   title: { fontSize: 22, fontWeight: '600' },
+=======
+  container: { flex: 1, paddingTop: 20, paddingHorizontal: 20 },
+  header: { marginBottom: 20 },
+  headerText: { fontSize: 28, fontWeight: "bold" },
+  box: { flexDirection: "row", backgroundColor: "#4A90E2", paddingVertical: 12, paddingHorizontal: 15, width: '100%', height: 60, borderRadius: 10, alignItems: "center", marginBottom: 15 },
+  boxText: { color: "#fff", fontWeight: "bold", marginLeft: 20 },
+  addButtonContainer: { alignItems: "center", marginTop: 30, marginBottom: 30 },
+  addButton: { width: 60, height: 60, borderRadius: 30, backgroundColor: "green", alignItems: "center", justifyContent: "center" },
+  addButtonText: { color: "#fff", fontSize: 28 },
+  modalContainer: { flex:1, justifyContent:'center', alignItems:'center', backgroundColor:'rgba(0,0,0,0.5)' },
+  modalContent: { width:'90%', backgroundColor:'white', padding:20, borderRadius:10 },
+  input: { borderWidth:1, borderColor:'#ccc', borderRadius:5, marginBottom:10, paddingHorizontal:10, height:40 },
+  modalButton: { backgroundColor:'green', padding:10, borderRadius:5, flex:1, alignItems:'center', marginHorizontal:5 },
+  selector: { borderWidth:1, borderColor:'#ccc', borderRadius:5, padding:10, marginBottom:10 }
+>>>>>>> Stashed changes
 });
